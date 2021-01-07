1df144fd8e26516741e69f8ee33b2dcfb8322619
Valerie <akpotevalerie@gmail.com>
2021-01-07 08:06:43 +0100

    Add seperate file for php convert button

--- index.php
+++ index.php
@@ -28,81 +28,11 @@
             </select><br><br>
             <input type="text" name="amount" value="" id="amount" placeholder="Enter amount ..." autocomplete="off"><br><br>
             <input type="submit" name="convert" value="Convert" id="convert-bttn">
-            <button type="button" id="swap-bttn">Swap</button>
+            <input type="submit" name="swap" value="Swap" id="swap-bttn">
         </form>
         <div id="display-conversion">
             <h6>Conversion:</h6>
-            <?php
-                ini_set('display_errors', '1');
-                ini_set('display_startup_errors', '1');
-                error_reporting(E_ALL);
-                
-                if (isset($_POST["convert"])) {
-                    $from_curr = $_POST["from"];
-                    $to_curr = $_POST["to"];
-                    $amount = $_POST["amount"];
-                    
-                    switch(true) {
-                        case ($amount==""):
-                            echo "<p class='colorMsg'>No amount to convert!</p>";
-                            break;
-                        case ($from_curr=="text" && $to_curr=="text"):
-                            echo "<p class='colorMsg'>Choose a currency!</p>";
-                            break;
-                        case ($from_curr=="text" && $to_curr=="twndllr"):
-                            echo "<p class='colorMsg'>Choose a currency!</p>";
-                            break;
-                        case ($from_curr=="text" && $to_curr=="usdllr"):
-                            echo "<p class='colorMsg'>Choose a currency!</p>";
-                            break;
-                        case ($from_curr=="text" && $to_curr=="euro"):
-                            echo "<p class='colorMsg'>Choose a currency!</p>";
-                            break;
-                        case ($from_curr=="twndllr" && $to_curr=="text"):
-                            echo "<p class='colorMsg'>Choose a currency!</p>";
-                            break;
-                        case ($from_curr=="twndllr" && $to_curr=="twndllr"):
-                            echo "<p>$amount TWD = $amount TWD</p>";
-                            break;
-                        case ($from_curr=="twndllr" && $to_curr=="usdllr"):
-                            $result = $amount*0.036;
-                            echo "<p>$amount TWD = $result USD</p>";
-                            break;
-                        case ($from_curr=="twndllr" && $to_curr=="euro"):
-                            $result = $amount*0.029;
-                            echo "<p>$amount TWD = $result EUR</p>";
-                            break;
-                        case ($from_curr=="usdllr" && $to_curr=="text"):
-                            echo "<p class='colorMsg'>Choose a currency!</p>";
-                            break;
-                        case ($from_curr=="usdllr" && $to_curr=="twndllr"):
-                            $result = $amount*27.98;
-                            echo "<p>$amount USD = $result TWD</p>";
-                            break;
-                        case ($from_curr=="usdllr" && $to_curr=="usdllr"):
-                            echo "<p>$amount USD = $amount USD</p>";
-                            break;
-                        case ($from_curr=="usdllr" && $to_curr=="euro"):
-                            $result = $amount*0.81;
-                            echo "<p>$amount USD = $result EUR</p>";
-                            break;
-                        case ($from_curr=="euro" && $to_curr=="text"):
-                            echo "<p class='colorMsg'>Choose a currency!</p>";
-                            break;
-                        case ($from_curr=="euro" && $to_curr=="twndllr"):
-                            $result = $amount*34.48;
-                            echo "<p>$amount EUR = $result TWD</p>";
-                            break;
-                        case ($from_curr=="euro" && $to_curr=="usdllr"):
-                            $result = $amount*1.23;
-                            echo "<p>$amount EUR = $result USD</p>";
-                            break;
-                        default:
-                            echo "<p>$amount EUR = $amount EUR</p>";
-                            break;   
-                    }
-                }
-            ?>
+            <?php require_once "convert.php";?>
         </div>
     </div>
 </body>
